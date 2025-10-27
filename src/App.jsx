cf505033d775897d8d73167464241f9ba6f7d094
mixomthombeni28-lgtm <mixomthombeni28@gmail.com>
2025-10-27 19:37:35 +0200

    fixed the customs booking page issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
-import CustomService from './pages/CustomService'; // <-- new import
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/booking" element={<BookingPage />} />
-            <Route path="/booking/custom" element={<CustomService />} /> {/* new route */}
           </Routes>
         </main>
       </div>
